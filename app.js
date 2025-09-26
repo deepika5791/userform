5e1a7b6092d1b8e645224ba60df9e88ed5a19e51
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-26 20:49:06 +0530

    added vercel links

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
-const cors = require(cors())
+const cors = require("cors");
 const routes = require("./src/userRouter/userRouter");
+app.use(cors());
 app.use(express.json());
 app.use("/form", routes);
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    vercel: "https://form-jade-nu.vercel.app/",
+  })
+);
 module.exports = app;
