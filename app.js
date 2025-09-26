90f4824a316267b4f1df0b7d4dcf822f74c3a883
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-26 22:26:47 +0530

    dublicate cors deleted

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("./src/userRouter/userRouter");
-app.use(cors());
-app.use(express.json());
-app.use("/form", routes);
 app.use(
   cors({
     origin: ["http://localhost:5173"],
     vercel: "https://form-jade-nu.vercel.app/",
   })
 );
+app.use(express.json());
+app.use("/form", routes);
 module.exports = app;
