8bbe7e2da4da9a72326cfc60a854c8f5e8c697a5
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-27 14:15:51 +0530

    added new vercel

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const routes = require("./src/userRouter/userRouter");
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://form-jade-nu.vercel.app"],
+    origin: ["http://localhost:5173", "https://form-tawny-seven.vercel.app"],
   })
 );
 app.options("*", cors());
