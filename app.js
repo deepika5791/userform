0b416ccbac74f7ecb831e16c572fdb259bd76f69
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-27 14:12:30 +0530

    deleted some code

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-// const express = require("express");
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const routes = require("./src/userRouter/userRouter");
-
-const app = express();
-
-// CORS setup
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://form-jade-nu.vercel.app"], // exact origins
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type"],
-  credentials: true,
-};
-
-// Use CORS globally
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://form-jade-nu.vercel.app"],
+  })
+);
+app.options("*", cors());
 app.use(express.json());
-
-// Handle preflight OPTIONS requests globally
-app.options("*", cors(corsOptions));
-
-// Mount your routes
 app.use("/form", routes);
-
 module.exports = app;
